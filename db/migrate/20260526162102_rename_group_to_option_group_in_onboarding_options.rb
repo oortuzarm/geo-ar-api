b649ab005e02da66b8e9eded5023b9f820845283
Osvaldo Ortuzar <ohortuzar@gmail.com>
2026-05-26 12:34:06 -0400

    Make onboarding option group migration resilient

--- db/migrate/20260526162102_rename_group_to_option_group_in_onboarding_options.rb
+++ db/migrate/20260526162102_rename_group_to_option_group_in_onboarding_options.rb
@@ -2,12 +2,16 @@ class RenameGroupToOptionGroupInOnboardingOptions < ActiveRecord::Migration[7.2]
   def change
     rename_column :onboarding_options, :group, :option_group
 
-    rename_index :onboarding_options,
-                 "index_onboarding_options_on_group",
-                 "index_onboarding_options_on_option_group"
+    if index_name_exists?(:onboarding_options, "index_onboarding_options_on_group")
+      rename_index :onboarding_options,
+                   "index_onboarding_options_on_group",
+                   "index_onboarding_options_on_option_group"
+    end
 
-    rename_index :onboarding_options,
-                 "index_onboarding_options_on_group_and_slug",
-                 "index_onboarding_options_on_option_group_and_slug"
+    if index_name_exists?(:onboarding_options, "index_onboarding_options_on_group_and_slug")
+      rename_index :onboarding_options,
+                   "index_onboarding_options_on_group_and_slug",
+                   "index_onboarding_options_on_option_group_and_slug"
+    end
   end
 end
