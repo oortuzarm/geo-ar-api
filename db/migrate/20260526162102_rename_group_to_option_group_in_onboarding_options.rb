8c8fa3a05207a5eed7ac4c4699cd376f935438cd
Osvaldo Ortuzar <ohortuzar@gmail.com>
2026-05-26 12:50:07 -0400

    Make onboarding rename migration idempotent

--- db/migrate/20260526162102_rename_group_to_option_group_in_onboarding_options.rb
+++ db/migrate/20260526162102_rename_group_to_option_group_in_onboarding_options.rb
@@ -1,17 +1,9 @@
 class RenameGroupToOptionGroupInOnboardingOptions < ActiveRecord::Migration[7.2]
   def change
-    rename_column :onboarding_options, :group, :option_group
+    if column_exists?(:onboarding_options, :group) &&
+       !column_exists?(:onboarding_options, :option_group)
 
-    if index_name_exists?(:onboarding_options, "index_onboarding_options_on_group")
-      rename_index :onboarding_options,
-                   "index_onboarding_options_on_group",
-                   "index_onboarding_options_on_option_group"
-    end
-
-    if index_name_exists?(:onboarding_options, "index_onboarding_options_on_group_and_slug")
-      rename_index :onboarding_options,
-                   "index_onboarding_options_on_group_and_slug",
-                   "index_onboarding_options_on_option_group_and_slug"
+      rename_column :onboarding_options, :group, :option_group
     end
   end
 end
