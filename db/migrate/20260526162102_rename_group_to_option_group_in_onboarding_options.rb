91c1d2d12485c5a1975c46d2fd73a3d78c09e089
Osvaldo Ortuzar <ohortuzar@gmail.com>
2026-05-26 12:26:05 -0400

    Rename onboarding_options group column to option_group

---
+++ db/migrate/20260526162102_rename_group_to_option_group_in_onboarding_options.rb
@@ -0,0 +1,13 @@
+class RenameGroupToOptionGroupInOnboardingOptions < ActiveRecord::Migration[7.2]
+  def change
+    rename_column :onboarding_options, :group, :option_group
+
+    rename_index :onboarding_options,
+                 "index_onboarding_options_on_group",
+                 "index_onboarding_options_on_option_group"
+
+    rename_index :onboarding_options,
+                 "index_onboarding_options_on_group_and_slug",
+                 "index_onboarding_options_on_option_group_and_slug"
+  end
+end
